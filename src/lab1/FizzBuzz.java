6e67917e23691501550111c4a8b8c20aae611e65
khizar-z <khiz.zaman@gmail.com>
2026-09-14 11:54:36 -0400

    i didnt actually refactor jack last time

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -3,7 +3,7 @@ package lab1;
 /**
  * Solve the lab1.FizzBuzz challenge.
  */
-class FizzBuzz {
+public class FizzBuzz {
 
     public static void main(String[] args) {
 
@@ -13,7 +13,7 @@ class FizzBuzz {
         }
     }
 
-    private static void doFizzBuzz(int i) {
+    public static void doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
---
+++ src/lab1/Main.java
@@ -0,0 +1,5 @@
+import lab1.FizzBuzz;
+
+static void main() {
+    FizzBuzz.doFizzBuzz(5);
+}
